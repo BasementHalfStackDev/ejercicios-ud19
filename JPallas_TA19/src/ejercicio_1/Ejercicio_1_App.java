8c3e36b683f9c4402befd800e950145864a775c6
Josep Maria Pallas Batalla <jpb_montblanc@hotmail.com>
2023-03-30 10:34:50 +0200

    Realizado ejercicio 1

--- JPallas_TA19/src/ejercicio_1/Ejercicio_1_App.java
+++ JPallas_TA19/src/ejercicio_1/Ejercicio_1_App.java
@@ -1,10 +1,21 @@
 package ejercicio_1;
 
+import java.awt.EventQueue;
+
 public class Ejercicio_1_App {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Ejercicio_1_GUI frame = new Ejercicio_1_GUI();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
 	}
 
 }
---
+++ JPallas_TA19/src/ejercicio_1/Ejercicio_1_GUI.java
@@ -0,0 +1,72 @@
+package ejercicio_1;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
+import java.awt.Font;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import javax.swing.AbstractAction;
+import java.awt.event.ActionEvent;
+import javax.swing.Action;
+import javax.swing.SwingConstants;
+import java.awt.SystemColor;
+import java.awt.Color;
+
+public class Ejercicio_1_GUI extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField txtAsd;
+	private final Action action = new SwingAction();
+
+
+	/**
+	 * Create the frame.
+	 */
+	public Ejercicio_1_GUI() {
+		setTitle("Hello! Whats your name?");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 290, 242);
+		contentPane = new JPanel();
+		contentPane.setBackground(new Color(204, 255, 255));
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+
+		JLabel lblNewLabel = new JLabel("Write a name to greet");
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setBounds(10, 38, 254, 17);
+		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		contentPane.add(lblNewLabel);
+
+		txtAsd = new JTextField();
+		txtAsd.setBounds(10, 97, 254, 20);
+		txtAsd.setToolTipText("Your name goes here");
+		contentPane.add(txtAsd);
+		txtAsd.setColumns(10);
+
+		JButton btnNewButton = new JButton("Greet me!");
+		btnNewButton.setBackground(new Color(255, 255, 153));
+		btnNewButton.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		btnNewButton.setAction(action);
+		btnNewButton.setBounds(78, 157, 121, 35);
+		contentPane.add(btnNewButton);
+	}
+
+	private class SwingAction extends AbstractAction {
+		public SwingAction() {
+			putValue(NAME, "Greet me!");
+			putValue(SHORT_DESCRIPTION, "Greets the user on click");
+		}
+
+		public void actionPerformed(ActionEvent e) {
+			JOptionPane.showMessageDialog(null, "Greetings " + txtAsd.getText() + "!");
+		}
+	}
+}
